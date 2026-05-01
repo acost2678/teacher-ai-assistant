98a09e5fe4c6264e1e75decd806174452b6e7853
acost2678 <jenacosta2678@gmail.com>
2026-05-01 11:21:52 -0400

    Add Google AdSense script

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,15 +26,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18116639889"></script>
-        <script dangerouslySetInnerHTML={{ __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'AW-18116639889');
-        `}} />
-      </head>
+     <head>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18116639889"></script>
+  <script dangerouslySetInnerHTML={{ __html: `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'AW-18116639889');
+  `}} />
+  <script
+    async
+    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9613352870851055"
+    crossOrigin="anonymous"
+  />
+</head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
